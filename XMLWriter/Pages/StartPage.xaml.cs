846dafacd6cbaaae9dc0668486ee6c9c32a977eb
Frozenenenen <85682798+Frozenenenen@users.noreply.github.com>
2021-08-15 01:48:36 +0200

    so'n Zeugs, das nicht funzt

--- XMLWriter/Pages/StartPage.xaml.cs
+++ XMLWriter/Pages/StartPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace XMLWriter.Pages
 {
     /// <summary>
@@ -21,6 +22,58 @@ namespace XMLWriter.Pages
         public StartPage()
         {
             InitializeComponent();
+            Language lingo = new Language();
+            DataSet data = new DataSet();
+            data.InitDataSet();
+            lingo.InitLanguage("Deutsch");
+
+            inputType.ItemsSource = data.GetDataTypeChoice();
+            inputType.Text = "rep";     //Aus irgendeinem Grund wird es nicht angezeigt, wenn gfs statt rep genutzt wird... Ich sag mal es reicht so
+            //btnWeiter.Content = lingo.GetStringNext();
+            btnWeiter.Content = "--->";
+
+
+            inputLanguage.ItemsSource = lingo.GetLanguageChoises();
+            inputLanguage.Text = lingo.GetStringLanguage();
+
+
+        }
+
+        private void Button_Next(object sender, RoutedEventArgs e)
+        {
+            //Ausgewähltes nächstes Fenster öffnen und dieses schließen
+            MainWindow main = new MainWindow();
+            RepPage rep = new RepPage();
+            GfsPage gfs1 = new GfsPage();
+            SavePage sW = new SavePage();
+            DataSet data = new DataSet();
+            data.SetDataType(inputType.Text);
+            Console.WriteLine("bla");
+            main.Main.Content = new RepPage();
+            if (inputType.Text == "rep")
+            {
+                main.Main.Content = new RepPage();
+            }
+            else if (inputType.Text == "gfs")
+            {
+                main.Main.Content = new GfsPage();
+            }
+            else
+            {
+                Console.WriteLine("Fehler in der gfs/rep-Wahl");
+            }
+
+
+            //Close();
+
+        }
+
+
+        private void btnSelectLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            Language language = new Language();
+            language.InitLanguage(inputLanguage.Text); //Befüllt die Variablen mit den Wörtern  der jeweiligen Sprache
+            textTitel.Content = language.GetStringCreateDataSet(); //Ausgabe der Überschrift "Create Data Set"
         }
     }
 }
