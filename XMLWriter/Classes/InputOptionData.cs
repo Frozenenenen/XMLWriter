b5866b5c70bc7ec4826a14dfe97197e6017cde0d
Frozenenenen <85682798+Frozenenenen@users.noreply.github.com>
2021-11-23 00:21:48 +0100

    Neue Objektklasse für Inputs

---
+++ XMLWriter/Classes/InputOptionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XMLWriter.Classes
+{
+    public class InputOptionData
+    {
+        public string tupelHalf1;
+        public string tupelHalf2;
+        public InputOptionData(string value1, string value2)
+        {
+            tupelHalf1 = value1;
+            tupelHalf2 = value2;
+        }
+    }
+}
